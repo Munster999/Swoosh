9f20a3883c997ee148aad82fb490bb35ef9fc9d0
Munster999 <20864076+Munster999@users.noreply.github.com>
2018-03-21 07:12:09 +0000

    Lesson 4 - Created another layout screen (activity_skill.xml) - Connected between layouts using 'leagueNetClicked' function - Updated 'strings.xml'

---
+++ app/src/main/java/com/munster/swoosh/BaseActivity.kt
@@ -0,0 +1,45 @@
+package com.munster.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+open class BaseActivity : AppCompatActivity() {
+
+    val TAG = "LifeCycle"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "${javaClass.simpleName} [ ON CREATE ]" )
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        Log.d(TAG, "${javaClass.simpleName} [ ON START ]" )
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "${javaClass.simpleName} [ ON RESUME ]" )
+        super.onResume()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG, "${javaClass.simpleName} [ ON RESTART ]" )
+        super.onRestart()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "${javaClass.simpleName} [ ON PAUSE ]" )
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "${javaClass.simpleName} [ ON STOP ]" )
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "${javaClass.simpleName} [ ON DESTROY ]" )
+        super.onDestroy()
+    }
+}

a15098d58f525e46a0fbc6ac15206e78509a4c1d
Munster999 <20864076+Munster999@users.noreply.github.com>
2018-03-21 11:05:02 +0000

    Lesson 4 - Created new 'constants.kt'file - Created logic for 'League' buttons to turn on\off etc. - Set up transfer from League activity to skills activity - Created variable for which 'league' button was pressed

---
+++ app/src/main/java/com/munster/swoosh/Constants.kt
@@ -0,0 +1,3 @@
+package com.munster.swoosh
+
+const val EXTRA_LEAGUE = "league"
--- app/src/main/java/com/munster/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/munster/swoosh/LeagueActivity.kt
@@ -3,17 +3,53 @@ package com.munster.swoosh
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
+    // configuring out the buttons on the 'LeagueActivity' page
+    fun onMensClicked(view: View) {
+        womensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        selectedLeague = "mens"
+    }
+
+    fun onWomensClicked(view: View) {
+        mensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        selectedLeague = "womens"
+    }
+
+    fun onCoedClicked(view: View) {
+        mensLeagueBtn.isChecked = false
+        womensLeagueBtn.isChecked = false
+
+        selectedLeague = "co-ed"
+    }
+
     // creating an intent from 'League Activity' to 'skill activity screen'
     fun leagueNextClicked(view: View) {
-        val skillActivity = Intent(this, SkillActivity::class.java)
-        startActivity(skillActivity)
+        if (selectedLeague != "") {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(skillActivity)
+        } else {
+            Toast.makeText(this, "Please select a League", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
+
+
+
 
 }

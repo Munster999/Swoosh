b1f1484919b732e4603c0f3740d6d1631310ba8a
Munster999 <20864076+Munster999@users.noreply.github.com>
2018-03-25 12:37:29 +0100

    Lesson 6 - Created model 'Player' class for parcelable data to be used throughout app - Created additional layouts for the following:        - Phone - Portrait        - Phone- Landscape        - Tablet - Portrait        - Tablet - Landscape

--- app/src/main/java/com/munster/swoosh/controller/LeagueActivity.kt
+++ app/src/main/java/com/munster/swoosh/controller/LeagueActivity.kt
@@ -57,7 +57,8 @@ class LeagueActivity : BaseActivity() {
     fun leagueNextClicked(view: View) {
         if (player.league != "") {
             val skillActivity = Intent(this, SkillActivity::class.java)
-            skillActivity.putExtra(EXTRA_PLAYER, player) // passing in an INSTANCE of the 'Player' class, which ahs the properties og 'league' and 'skill' inside it.
+            skillActivity.putExtra(EXTRA_PLAYER, player)
+            // passing in an INSTANCE of the 'Player' class, which has the properties of 'league' and 'skill' inside it.
             startActivity(skillActivity)
         } else {
             Toast.makeText(this, "Please select a League", Toast.LENGTH_SHORT).show()
